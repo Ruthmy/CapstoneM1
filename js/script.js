0e80c6f40c4a612f225b603e5b3456a2d49b6a71
Ruthmy <Ruryemo.exe@gmail.com>
2023-03-20 14:27:34 -0600

    Created the first part of mobile version - Created the home structure on the index.html - Created the basic styles for all pages on the style.css file - Created the home styles on the home.css file - Menu mobile and desktop already creating and working - Added all the images and background for the page sections

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-// Empty JavaScript file
+// Code for Mobile Menu
+const hamburger = document.querySelector('#hamburger');
+const menu = document.querySelector('.menu');
+const arrAncherTag = document.querySelectorAll('.menu__link');
+const mediaQuery = window.matchMedia('(max-width: 768px)');
+
+hamburger.addEventListener('click', (event) => {
+  event.stopPropagation();
+  menu.classList.toggle('showMobil');
+  hamburger.classList.toggle('showMobil');
+});
+
+arrAncherTag.forEach((a) => {
+  a.addEventListener('click', (event) => {
+    event.stopPropagation();
+    if (mediaQuery.matches) {
+      menu.classList.toggle('showMobil');
+      hamburger.classList.toggle('showMobil');
+    }
+  });
+});
+
+
